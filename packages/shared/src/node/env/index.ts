80f4c8e444eb0e222db98e6ad7a3f45fefba7a3a
Luka Hedt <luka.hedt@service.vic.gov.au>
2026-03-23 12:50:01 +1100

    fix: Let seed command read DB Variables properly

--- packages/shared/src/node/env/index.ts
+++ packages/shared/src/node/env/index.ts
@@ -1,3 +1,4 @@
 export { default as UrlSet } from './UrlSet.js';
 export { default as GlobalValues, parseTimeoutEnv } from './GlobalValues.js';
 export { default as ConsoleLog } from './ConsoleLog.js';
+export { buildDatabaseUrl } from './GlobalValues.js';
